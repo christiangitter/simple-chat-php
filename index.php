3fe135f3d33f273343c93b1aeb4cb0ee1162d66d
Christian Gitter <c.gitter@heurich.de>
2021-08-10 13:23:04 +0200

    init files

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    
+    <title>PLC | Pretty Little Chat</title>
+    
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+    
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script type="text/javascript" src="js/chat.js"></script>
+    
+</head>
+
+<body onload="setInterval('chat.update()', 1000)">
+
+    <div id="page-wrap">
+    
+        <h2>PLC | Pretty Little Chat</h2>
+  
+        
+        <p id="nickarea"></p>
+        
+        <div id="chat-wrap"><div id="chat-area"></div></div>
+        
+        <div class="sendTextArea">
+        <form id="send-message-area">
+            <p>Deine Nachricht: </p>
+            <textarea id="sendie" maxlength = '100' ></textarea>
+        </form>
+        </div>
+    </div>
+    <script type="text/javascript" src="js/intro.js"></script>
+</body>
+
+</html>
